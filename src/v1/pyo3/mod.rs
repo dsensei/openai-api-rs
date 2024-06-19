b2b7fe223ed4df43cf68128ecd1db37809e0728f
Yulong Liu <lyl890808@gmail.com>
2024-06-19 00:23:00 -0700

    remove pyo3 dependency from Cargo.toml and src/v1/pyo3/mod.rs

--- src/v1/pyo3/mod.rs
+++ src/v1/pyo3/mod.rs
@@ -1,21 +1,8 @@
-use pyo3::{types::PyModule, FromPyObject, IntoPy, PyObject, Python};
 use serde::{Deserialize, Serialize};
 
-
-#[derive(FromPyObject, Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct LoraRequest {
     pub lora_id: String,
     pub lora_int_id: i32,
     pub lora_local_path: String,
-}
-
-impl IntoPy<PyObject> for LoraRequest {
-    fn into_py(self, py: Python) -> PyObject {
-        let py_module = PyModule::import(py, "vllm.lora.request").unwrap();
-        let py_class = py_module.getattr("LoRARequest").unwrap();
-        let lora_request = py_class
-            .call1((self.lora_id, self.lora_int_id, self.lora_local_path))
-            .unwrap();
-        lora_request.into_py(py)
-    }
 }
